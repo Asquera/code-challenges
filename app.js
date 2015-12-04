5580c5259999ba108660fe2bc2331f1c123c3775
BriocheBerlin <brigitte.markmann@asquera.de>
2015-12-04 15:36:29 +0100

    Adds sample data

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var codeChallengesApp = angular.module('codeChallengesApp', []);
 
 codeChallengesApp.controller('ExcerciseListCtrl', function ($scope) {
   $scope.excercises = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+    {'name': 'SSH key konfigurieren',
+     'snippet': 'Distillery irony neutra organic fugiat. Meditation occaecat keytar cornhole, mixtape next level blue bottle et meh stumptown messenger bag mlkshk.'},
+    {'name': 'Fizzbuzz',
+     'snippet': 'Heirloom yr esse, pug beard VHS excepteur artisan sed photo booth intelligentsia. Qui sapiente +1 90\'s, occupy vegan williamsburg ea velit magna yuccie elit.'},
+    {'name': 'Dotfiles einrichten',
+     'snippet': 'Distillery cray pop-up semiotics minim laboris. Twee you probably haven\'t heard of them locavore, blue bottle DIY farm-to-table kickstarter hoodie pabst aliqua lomo.'}
   ];
 });
